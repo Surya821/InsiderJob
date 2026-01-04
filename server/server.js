0ae110c0e18e9a019dd9885e2508999ea52ed36a
Surya Pratap Singh <surya30082005@gmail.com>
2026-01-04 21:35:41 +0530

    backend fix

--- server/server.js
+++ server/server.js
@@ -45,6 +45,6 @@ const PORT = process.env.PORT || 5000
 
 Sentry.setupExpressErrorHandler(app);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`);
-})
+// app.listen(PORT, ()=>{
+//     console.log(`Server is running on ${PORT}`);
+// })
